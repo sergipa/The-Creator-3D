562831b39015e70dc34565457cfe4b52ce942359
TinoTano <elsuperchowchow@gmail.com>
2017-09-25 00:16:48 +0200

    added gpu details in hardware window

--- HardwareWindow.cpp
+++ HardwareWindow.cpp
@@ -1,5 +1,7 @@
 #include "HardwareWindow.h"
 #include "SDL\include\SDL.h"
+//#include "SDL\include\SDL_opengl.h"
+#include "OpenGL.h"
 
 HardwareWindow::HardwareWindow()
 {
@@ -46,6 +48,75 @@ void HardwareWindow::DrawWindow()
 	if (SDL_HasSSE41) str += "SSE41, ";
 	if (SDL_HasSSE41) str += "SSE42";
 	ImGui::TextColored(ImVec4(1, 1, 0, 1), "%s", str.c_str());
+	ImGui::Separator();
+	const GLubyte* gpu_vendor = glGetString(GL_VENDOR);
+	const GLubyte* gpu_renderer = glGetString(GL_RENDERER);
+	const GLubyte* gpu_version = glGetString(GL_VERSION);
+	ImGui::Text("GPU Vendor: ");
+	ImGui::SameLine();
+	ImGui::TextColored(ImVec4(1, 1, 0, 1), "%s", gpu_vendor);
+	ImGui::Text("GPU Model: ");
+	ImGui::SameLine();
+	ImGui::TextColored(ImVec4(1, 1, 0, 1), "%s", gpu_renderer);
+	ImGui::Text("GPU Driver: ");
+	ImGui::SameLine();
+	ImGui::TextColored(ImVec4(1, 1, 0, 1), "%s", gpu_version);
+	if (GLEW_NVX_gpu_memory_info)
+	{
+		int availableKB;
+		int totalKB;
+		int dedicatedKB;
+		int evictedKB;
+		int evicted_counts;
+		glGetIntegerv(GL_GPU_MEMORY_INFO_CURRENT_AVAILABLE_VIDMEM_NVX, &availableKB);
+		glGetIntegerv(GL_GPU_MEMORY_INFO_TOTAL_AVAILABLE_MEMORY_NVX, &totalKB);
+		glGetIntegerv(GL_GPU_MEMORY_INFO_DEDICATED_VIDMEM_NVX, &dedicatedKB);
+		glGetIntegerv(GL_GPU_MEMORY_INFO_EVICTED_MEMORY_NVX, &evictedKB);
+		glGetIntegerv(GL_GPU_MEMORY_INFO_EVICTION_COUNT_NVX, &evicted_counts);
 
+		ImGui::Text("Total VRAM: ");
+		ImGui::SameLine();
+		ImGui::TextColored(ImVec4(1, 1, 0, 1), "%dMb", totalKB / 1024);
+		ImGui::Text("Available VRAM: ");
+		ImGui::SameLine();
+		ImGui::TextColored(ImVec4(1, 1, 0, 1), "%dMb", availableKB / 1024);
+		ImGui::Text("Dedicated VRAM: ");
+		ImGui::SameLine();
+		ImGui::TextColored(ImVec4(1, 1, 0, 1), "%dMb", dedicatedKB / 1024);
+		ImGui::Text("Evicted Memory: ");
+		ImGui::SameLine();
+		ImGui::TextColored(ImVec4(1, 1, 0, 1), "%dMb", evictedKB / 1024);
+		ImGui::Text("Evicted Memory Counts: ");
+		ImGui::SameLine();
+		ImGui::TextColored(ImVec4(1, 1, 0, 1), "%d", evicted_counts / 1024);
+	}
+	else if (GLEW_ATI_meminfo)
+	{
+		int texture_memory[4];
+		int vbo_memory[4];
+		int renderbuffer_memory[4];
+		glGetIntegerv(GL_TEXTURE_FREE_MEMORY_ATI, texture_memory);
+		glGetIntegerv(GL_VBO_FREE_MEMORY_ATI, vbo_memory);
+		glGetIntegerv(GL_RENDERBUFFER_FREE_MEMORY_ATI, renderbuffer_memory);
+		ImGui::Text("Textures Memory:");
+		ImGui::BulletText("Total free memory: %dMB", texture_memory[0] / 1024);
+		ImGui::BulletText("Largest available dedicated memory free block: %dMB", texture_memory[1] / 1024);
+		ImGui::BulletText("Total shared free system memory: %dMB", texture_memory[2] / 1024);
+		ImGui::BulletText("Largest shared free system memory block: %dMB", texture_memory[3] / 1024);
+		ImGui::Text("VBO Memory:");
+		ImGui::BulletText("Total free memory: %dMB", vbo_memory[0] / 1024);
+		ImGui::BulletText("Largest available dedicated memory free block: %dMB", vbo_memory[1] / 1024);
+		ImGui::BulletText("Total shared free system memory: %dMB", vbo_memory[2] / 1024);
+		ImGui::BulletText("Largest shared free system memory block: %dMB", vbo_memory[3] / 1024);
+		ImGui::Text("Renderbuffer Memory:");
+		ImGui::BulletText("Total free memory: %dMB", renderbuffer_memory[0] / 1024);
+		ImGui::BulletText("Largest available dedicated memory free block: %dMB", renderbuffer_memory[1] / 1024);
+		ImGui::BulletText("Total shared free system memory: %dMB", renderbuffer_memory[2] / 1024);
+		ImGui::BulletText("Largest shared free system memory block: %dMB", renderbuffer_memory[3] / 1024);
+	}
+	else
+	{
+		ImGui::TextColored(ImVec4(1, 0, 0, 1), "Intel GPU memory details not available");
+	}
 	ImGui::End();
 }
